bb67fae981b9ab41f4bf66a8ae8c6dcbfa3f5b1d
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-10-17 10:30:16 +0300

    Style the custom login page (signin.js) and add the callbackurl in the button to push user to home page on successful login

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,15 +1,34 @@
 import { getProviders, signIn as SignInProvider } from "next-auth/react";
+import Header from "../../components/Header";
 
 export default function SignIn({ providers }) {
   return (
     <>
-      {Object.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button onClick={() => SignInProvider(provider.id)}>
-            Sign in with {provider.name}
-          </button>
+      <Header />
+      <section className="flex flex-col items-center justify-center min-h-screen py-2 -mt-36 px-14 text-center">
+        <img
+          className="w-80"
+          src="https://links.papareact.com/ocw"
+          alt="instagram icon"
+        />
+        <p className="text-red-400">
+          This is not a real app, it is built for educational purposes only.
+        </p>
+        <div className="mt-20">
+          {Object.values(providers).map((provider) => (
+            <div className="" key={provider.name}>
+              <button
+                className="p-3 bg-blue-500 rounded-lg text-white"
+                onClick={() =>
+                  SignInProvider(provider.id, { callbackUrl: "/" })
+                }
+              >
+                Sign in with {provider.name}
+              </button>
+            </div>
+          ))}
         </div>
-      ))}
+      </section>
     </>
   );
 }
@@ -21,4 +40,3 @@ export async function getServerSideProps() {
     props: { providers },
   };
 }
-
