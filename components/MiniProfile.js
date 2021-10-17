8d732925fd9bac860a1424d5aceea1c65886c6c8
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-10-17 13:14:24 +0300

    conditionally render suggestions and minicomponent to only show when user is logged in

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -1,16 +1,22 @@
+import { signOut, useSession } from "next-auth/react";
+
 const MiniProfile = () => {
+  const { data: session } = useSession();
+  console.log(session);
   return (
     <section className="flex items-center justify-between mt-14 ml-10">
       <img
         className="rounded-full border p-[2px] w-16 h-16"
-        src="https://links.papareact.com/3ke"
-        alt="user profile"
+        src={session?.user?.image}
+        alt=" "
       />
       <div className="flex-1 mx-4">
-        <h2 className="font-bold"> humphreyMutuma</h2>
+        <h2 className="font-bold">{session?.user?.username}</h2>
         <h3 className="text-sm text-gray-400">Welcome to instagram</h3>
       </div>
-      <button className="text-sm font-semibold text-blue-400">Sign out</button>
+      <button onClick={signOut} className="text-sm font-semibold text-blue-400">
+        Sign out
+      </button>
     </section>
   );
 };
