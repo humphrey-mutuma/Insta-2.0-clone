352c5bcc474f3b8e950fdc31b30f18feb3f529ce
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-10-18 12:35:51 +0300

    Conditionally render like buttons and comments form when a user is logged in.

--- components/Post.js
+++ components/Post.js
@@ -7,8 +7,11 @@ import {
   PaperAirplaneIcon,
 } from "@heroicons/react/outline";
 import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
+import { useSession } from "next-auth/react";
 
 const Post = ({ id, username, userImg, img, caption }) => {
+  const { data: session } = useSession();
+
   return (
     <section className="bg-white my-7 border rounded-sm">
       {/* header */}
@@ -24,16 +27,21 @@ const Post = ({ id, username, userImg, img, caption }) => {
       {/* image */}
       <img src={img} alt="post image" className="object-cover w-full " />
       {/* buttons */}
-      <section className="flex justify-between items-center px-4 pt-4">
-        <div className="flex space-x-4 ">
-          <HeartIcon className="btn" />
-          <ChatIcon className="btn" />
-          <PaperAirplaneIcon className="btn" />
-        </div>
-        <div>
-          <BookmarkIcon className="btn" />
-        </div>
-      </section>
+
+      {/* conditionally show like buttons etc when user is logged in */}
+
+      {session && (
+        <section className="flex justify-between items-center px-4 pt-4">
+          <div className="flex space-x-4 ">
+            <HeartIcon className="btn" />
+            <ChatIcon className="btn" />
+            <PaperAirplaneIcon className="btn" />
+          </div>
+          <div>
+            <BookmarkIcon className="btn" />
+          </div>
+        </section>
+      )}
       {/* caption  */}
       <p className="p-5 truncate">
         <span className="font-bold mr-1">{username}</span>
@@ -41,15 +49,18 @@ const Post = ({ id, username, userImg, img, caption }) => {
       </p>
       {/* comments */}
       {/* input box */}
-      <form className="flex p-4 items-center">
-        <EmojiHappyIcon className="h-7" />
-        <input
-          placeholder="Add a comment"
-          className="flex-1 border-none focus:ring-0 outline-none"
-          type="text"
-        />
-        <button className="font-semibold text-blue-400">Post</button>
-      </form>
+      {/* conditionally show comment button and input when user in logged in */}
+      {session && (
+        <form className="flex p-4 items-center">
+          <EmojiHappyIcon className="h-7" />
+          <input
+            placeholder="Add a comment"
+            className="flex-1 border-none focus:ring-0 outline-none"
+            type="text"
+          />
+          <button className="font-semibold text-blue-400">Post</button>
+        </form>
+      )}
     </section>
   );
 };
